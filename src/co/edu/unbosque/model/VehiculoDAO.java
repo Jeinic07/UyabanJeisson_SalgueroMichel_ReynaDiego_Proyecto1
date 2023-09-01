55423d0f275b726a014e7490aae0791b4a074cfa
Jeinic07 <122488468+Jeinic07@users.noreply.github.com>
2023-09-01 00:41:00 -0500

    PENE 2

--- src/co/edu/unbosque/model/VehiculoDAO.java
+++ src/co/edu/unbosque/model/VehiculoDAO.java
@@ -10,6 +10,7 @@ public class VehiculoDAO implements OperacionesDAO {
 	private MyLinkedList<VehiculoDTO> vehiculos;
 
 	public VehiculoDAO() {
+//		vehiculos = new MyLinkedList<VehiculoDTO>();
 
 		try {
 			vehiculos = (MyLinkedList<VehiculoDTO>) Serializacion.readSerializable("Vehiculo.txt");
